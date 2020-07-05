afded7c86c178b632ed8f96bfa8479f034c30dc0
Aleksandr Ampleev <Aleksandr_Ampleev@epam.com>
2020-07-05 15:49:27 +0400

    add repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,57 @@
-const CustomError = require("../extensions/custom-error");
+﻿const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+
+
+//### ** Расширенный повторитель **
+
+//    Ваша задача — реализовать функцию`repeater(str, options)`.
+//Эта функция возвращает повторяющуюся ** строку **, основываясь на заданных параметрах:
+//* `str` это ** строка **, которая будет повторена
+//    * `options` это ** объект ** опций, который содержит следующие свойства:
+//-	`repeatTimes` устанавливает число повторений`str`
+//- `separator` это строка, разделяющая повторения`str`
+//- `addition` это дополнительная строка, которая будет добавлена после каждого повторения`str`
+//- `additionRepeatTimes` устанавливает число повторений`addition`
+//- `additionSeparator` это строка, разделяющая повторения`addition`
+
+//Параметры`str` и`addition` по умолчанию являются ** строками **.В случае, если они другого типа, он должны быть преобразованы к строке.
+
+//  Параметры`separator` и`additionSeparator` являются строками.
+
+//`repeatTimes` и`additionRepeatTimes` являются целыми числами(в случае отсутствия любого из них соответствующая строка не повторяется).
+
+//Единственный обязательный параметр — это`str`, остальные могут не быть переданы.
+//    Значение`separator` по умолчанию это`'+'`.Значение`additionSeparator` по умолчанию это`'|'`.
+
+//        Например: `repeater('STRING', { repeatTimes: 3, separator: '**', addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })` => `'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'`
+
+//Напишите свой код в`src/extended-repeater.js`.
+
+//alert(repeater('string', { repeatTimes: 3, separator: '**', addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' }));
+//alert(repeater('String', { repeatTimes: 5, separator: ';', addition: '!', additionRepeatTimes: 2, additionSeparator: 'm' }));
+
+module.exports = function repeater(str, options) {
+    var retStr = [];
+    if (options) {
+        if (options.repeatTimes) {
+            for (var i = 0; i < options.repeatTimes; i++) {
+                retStr.push(str);
+                if (options.addition && options.additionRepeatTimes > 0) {
+
+                    for (var j = 0; j < options.additionRepeatTimes; j++) {
+                        retStr.push(options.addition);
+                        if (options.additionSeparator && j < (options.additionRepeatTimes - 1)) {
+                            retStr.push(options.additionSeparator);
+                        }
+                    }
+                }
+
+                if (options.separator && i < (options.repeatTimes - 1)) {
+                    retStr.push(options.separator);
+                }
+            }
+        }
+    }
+    return retStr.join('');
 };
   
