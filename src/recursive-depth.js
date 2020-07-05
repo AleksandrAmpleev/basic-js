f4911b90ab0560d718843d5870049553d91af2a3
Aleksandr Ampleev <Aleksandr_Ampleev@epam.com>
2020-07-05 22:53:50 +0400

    add depth calculator and  whar-season.

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,37 @@
-const CustomError = require("../extensions/custom-error");
+﻿const CustomError = require("../extensions/custom-error");
+
+//### ** Рекурсивный вычислитель глубины **
+//    ![Идти глубже](https://i.imgur.com/k7lADiM.jpg)
+
+//        Ваша задача — реализовать класс`DepthCalculator` с методом`calculateDepth`, который принимает массив и возвращает его ** глубину **.
+
+//            Метод`calculateDepth` должен проходить полученный массив ** рекурсивно **.Глубина ** плоского ** массива — 1. Метод должен корректно работать с массивами, не содержащими элементов или содержащими пустые массивы.
+
+//            Например:
+
+//        `const depthCalc = new DepthCalculator();`
+//            `const { calculateDepth } = depthCalc;`
+
+//            `calculateDepth([1, 2, 3, 4, 5])` => `1`
+
+//            `calculateDepth([1, 2, 3, [4, 5]])` => `2`
+
+//            `calculateDepth([[[]]])` => `3`
+
+//Напишите ваш код в`src/recursive-depth.js`.
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    calculateDepth(arr, currentDepth) {
+        var depth = currentDepth == undefined ? 1 : currentDepth;
+        var maxDebth = depth;
+
+        for (var i = 0; i < arr.length; i++) {
+            if (arr[i] instanceof Array) {
+                depth += 1;
+                var localDepth = calculateDepth(arr[i], depth);
+                if (maxDebth < localDepth) { maxDebth = localDepth; }
+            }
+        }
+        return maxDebth;
+    }
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,44 @@
-const CustomError = require("../extensions/custom-error");
+﻿const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+//### ** Какая пора года ??**
+//Ваша задача — реализовать функцию`getSeason(date)`, которая принимает объект`Date` и возвращает соответствующую ему пору года.Пора года должна быть типа`string`.
+//Если аргумент`date` не был передан, функция должна вернуть строку`'Unable to determine the time of year!'` Если аргумент`date` ** некорректный **, функция должна выбросить ошибку.
+//Тссс! Среди аргументов, которые попадают в эту функцию, затесался вражеский агент.
+//![Disguised](https://www.famousbirthdays.com/faces/disguised-toast-image.jpg)
+//Он руководствуется знаменитой поговоркой: "Если это выглядит как **утка**, плавает как **утка**, и крякает как **утка**, тогда это, скорее всего, **утка** (и неважно, что это **на самом деле**)".
+//Он ** искусно маскируется ** под настоящую дату(тип`date`), но умелый javascript - разработчик может поймать его и выбросить ошибку как раз вовремя!
+//Напишите ваш код в`src/what-season.js`.
+
+module.exports = function getSeason(date) {
+    function getSeason(date) {
+        if (!(date instanceof Date)) {
+            throw new TypeError('type error. expected date');
+        }
+
+        var t = date.getMonth();
+        var season = "";
+        switch (t) {
+            case 0:
+            case 1:
+            case 11:
+                season = 'winter';
+                break;
+            case 2:
+            case 3:
+            case 4:
+                season = 'spring';
+                break;
+            case 5:
+            case 6:
+            case 7:
+                season = 'summer';
+                break;
+            case 8:
+            case 9:
+            case 10:
+                season = 'autumn';
+                break;
+        }
+        return season;
+    };
 };
