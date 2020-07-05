9233491ece879e6734124c69faf84cdfa98e2986
Aleksandr Ampleev <Aleksandr_Ampleev@epam.com>
2020-07-05 14:34:15 +0400

    add count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,20 @@
-const CustomError = require("../extensions/custom-error");
+﻿const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+//### ** Сосчитай котов! **
+
+//    Ваша задача — сосчитать котов, спрятавшихся на заднем дворе(представленном в виде двумерного массива, `Array`).
+//Коты прячутся хорошо, но их ** ушки ** (`"^^"`) видны очень хорошо.Ваша задача — реализовать функцию`countCats(backyard)`, которая сосчитает котов.Удачи!
+
+//Число найденных котов должно иметь тип`number`.Если коты не найдены, функция должна вернуть`0`.
+
+//Напишите ваш код в`src/count-cats.js`.
+
+module.exports = function countCats(matrix) {
+    var cCats = 0;
+    for (var i = 0; i < matrix.length; i++) {
+        let row = matrix[i].join(';');
+        var t = row.split('^^');
+        cCats += t.length - 1;
+    }
+    return cCats;
 };
