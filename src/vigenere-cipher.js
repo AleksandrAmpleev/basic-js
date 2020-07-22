75fc1cfd921ed98de5fd33095d5c6477fce656c9
Aleksandr Ampleev <Aleksandr_Ampleev@epam.com>
2020-07-23 01:36:54 +0400

    Fix bugs in vigenere-cipher task. Implemented reverse mashine.

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,51 +20,53 @@
 //---
 
 class VigenereCipheringMachine {
-    encrypt(message, key) {
+    constructor(direction) {
+        this.directionReverse = direction !== false;
         this.engLang = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toLowerCase();
-        //=========================================================================
-        var tabulaRecta = [];
-        for (var i = 0; i < 26; i++) {
+
+        this.tabulaRecta = [];
+        for (let i = 0; i < 26; i++) {
             var shift = i - 26;
             if (shift < 0) {
                 shift = shift * -1;
             }
-            tabulaRecta[i] = new Array();
-            for (var j = 0; j < 26; j++) {
+            this.tabulaRecta[i] = new Array();
+            for (let j = 0; j < 26; j++) {
                 if ((26 - shift) > 25) {
                     shift = 26;
                 }
-                tabulaRecta[i].push(this.engLang[26 - shift]);
+                this.tabulaRecta[i].push(this.engLang[26 - shift]);
                 shift--;
             }
         }
-        /*return tabulaRecta;*/
-        //=========================================================================
+
+    }
+    encrypt(message, key) {
+        let msg = message.toLowerCase();
         var simbolsCount = 0;
-        for (var i = 0; i < message.length; i++) {
-            if (this.engLang.indexOf(message[i]) !== -1) {
+        for (let i = 0; i < msg.length; i++) {
+            if (this.engLang.indexOf(msg[i]) !== -1) {
                 simbolsCount++;
             }
         }
 
-        var needLnght = message.length;//Math.ceil(simbolsCount / key.length);
+        var needLnght = msg.length;
         var str = [];
         for (var i = 0; i < needLnght; i++) {
             str.push(key);
         }
-        var extendedKey = str.join('').slice(0, message.length); //return str.join('').slice(0, simbolsCount);
-        //=========================================================================
+        var extendedKey = str.join('').slice(0, msg.length);
 
         var shifr = [];
         var lastIndexSource = 0;
         var lastIndexKey = 0;
 
-        for (var i = 0; i < message.length; i++) {
-            var literaSource = message[lastIndexSource].toLowerCase();
+        for (let i = 0; i < msg.length; i++) {
+            var literaSource = msg[lastIndexSource].toLowerCase();
             var literaKey = extendedKey[lastIndexKey].toLowerCase();
 
-            var column = this.engLang.indexOf(literaSource);// GetIndex(literaSource, this.engLang);
-            var row = this.engLang.indexOf(literaKey); //GetIndex(literaKey, this.engLang);
+            var column = this.engLang.indexOf(literaSource);
+            var row = this.engLang.indexOf(literaKey);
 
             lastIndexSource++;
 
@@ -76,71 +78,63 @@ class VigenereCipheringMachine {
 
             lastIndexKey++;
 
-            var shifrLitera = tabulaRecta[row][column];
+            var shifrLitera = this.tabulaRecta[row][column];
             shifr.push(shifrLitera);
         }
-        return shifr.join('').toUpperCase();     
+        if (this.directionReverse) {
+            return shifr.join('').toUpperCase();
+        } else {
+            return shifr.reverse().join('').toUpperCase();
+        }
     }
 
- 
-    decrypt(message, key) {
-        this.engLang = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toLowerCase();
-        //=========================================================================
-        var tabulaRecta = [];
-        for (var i = 0; i < 26; i++) {
-            var shift = i - 26;
-            if (shift < 0) {
-                shift = shift * -1;
-            }
-            tabulaRecta[i] = new Array();
-            for (var j = 0; j < 26; j++) {
-                if ((26 - shift) > 25) {
-                    shift = 26;
-                }
-                tabulaRecta[i].push(this.engLang[26 - shift]);
-                shift--;
-            }
-        }
-        //=========================================================================
 
+    decrypt(message, key) {
+        let msg = message.toLowerCase();
         var originalText = [];
         var lastMessageLitera = 0;
 
-        var extKey = key.repeat(Math.ceil(message.length / key.length) + 1);
+        var extKey = key.repeat(Math.ceil(msg.length / key.length) + 1);
 
-        for (var i = 0; i < extKey.length; i++) {
+        for (let i = 0; i < extKey.length; i++) {
             var literaKey = extKey[i].toLowerCase();
 
+            let start = msg[lastMessageLitera];
 
-            for (var j = 0; j < tabulaRecta.length; j++) {
-                if (tabulaRecta[j][0] === literaKey && message.length > lastMessageLitera) {
-                    var start = message[lastMessageLitera].toLowerCase();
-                    if (!tabulaRecta[j].includes(start)) {
-                        originalText.push(start);
-                        lastMessageLitera++;
-
-                        if (message.length === lastMessageLitera) {
-                            break;
-                        }
-
-                        start = message[lastMessageLitera].toLowerCase();
-                    }
-
-                    for (var z = 0; z < tabulaRecta[j].length; z++) {
-                        if (tabulaRecta[j][z] === start) {
-                            start = this.engLang[z];
-                            break;
-                        }
-                    }
-
-                    originalText.push(start);
-                    lastMessageLitera++;
+            while (!this.engLang.includes(start)) {
+                originalText.push(start);
+                lastMessageLitera++;
+                start = msg[lastMessageLitera];
+                if (start === undefined) {
                     break;
                 }
             }
+
+            if (msg.length < lastMessageLitera || start === undefined) {
+                break;
+            }
+
+            for (let j = 0; j < this.tabulaRecta.length; j++) {
+
+                if (this.tabulaRecta[j][0] === literaKey) {
+                    for (let z = 0; z < this.tabulaRecta[j].length; z++) {
+                        if (this.tabulaRecta[j][z] === start) {
+                            start = this.engLang[z];
+                            originalText.push(start);
+                            lastMessageLitera++;
+                            break;
+                        }
+                    }
+                }
+
+            }
         }
-        
-        return originalText.join('').toUpperCase();
+
+        if (this.directionReverse) {
+            return originalText.join('').toUpperCase();
+        } else {
+            return originalText.reverse().join('').toUpperCase();
+        }
     }
 }
 
